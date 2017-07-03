dccba9ea3b6a887eadd1e3d0a53b2fec7f667aad
ginocremer <gino@pixelbar.be>
2017-07-03 12:19:21 +0200

    Update page.php

--- kursinhalte/2 - Ein erstes kleines WordPress-Theme/page.php
+++ kursinhalte/2 - Ein erstes kleines WordPress-Theme/page.php
@@ -1,4 +1,14 @@
+
+
+<?php get_header('blog'); ?>
+
+
 <h1>page.php - Ich bin eine ganz normale Seite</h1>
 <p>
   Dieses Template ist ein Fallback für alle Seiten. Es kann natürlich mit einem spezifischeren Seiten-Template wie page-1.php übergangen werden. 
 </p>
+<?php get_sidebar('links'); ?>
+
+
+<?php get_footer('rot');
+
